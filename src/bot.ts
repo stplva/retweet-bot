e71e22e6c5d24defea3af6214245840d411e9cb4
Sofiia Teplova <sofiia.teplova@gmail.com>
2023-04-05 22:51:51 +0200

    Refactor code; add prompt

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,6 @@
 // environmental variables setup
 require('dotenv').config()
+const prompt = require("prompt-sync")()
 
 import {
 	getTweetsById,
@@ -17,63 +18,67 @@ const weights: Weights = {
 	like_weight: 1,
 }
 
+
+const getTweet = async (ids: any) => {
+	try {
+		const res = await getTweetsById(ids)
+		console.log(res)
+		return res
+	} catch (e) {
+		console.log(e)
+		process.exit(-1)
+	}
+}
+
+const getRecentTweets = async (query: string, days?: number) => {
+	try {
+		const res = await searchRecentTweetsByQuery(query, days)
+		return res
+	} catch (e) {
+		console.log(e)
+		process.exit(-1)
+	}
+}
+
+const getMostPopTweetId = (tweets: Tweet[] | undefined) => {
+	console.log(`getMostPopTweetId`)
+
+	if (!tweets) {
+		console.log('getMostPopTweetId: tweets are undefined', tweets)
+		return
+	}
+
+	const ratingArr = tweets.map((tweet) => {
+		const rating = calcRating(tweet.public_metrics, weights)
+		return { id: tweet.id, rating }
+	})
+
+	const maxRating = ratingArr.reduce((acc: any, el: any) => {
+		return (acc = acc > el.rating ? acc : el.rating)
+	}, 0)
+
+	if (maxRating === 0) {
+		console.log('There were no tweets worth retweeting today.')
+		return
+	}
+
+	const mostPopTweet = ratingArr.find((el) => el.rating === maxRating)
+
+	console.log(ratingArr)
+	console.log(`The most popular tweet: ${mostPopTweet?.id} with a rating: ${maxRating}`)
+	return mostPopTweet?.id
+}
+
 export const Bot = async () => {
 	console.log('Bot started...')
 
-	const getTweet = async (ids: any) => {
-		try {
-			const res = await getTweetsById(ids)
-			console.log(res)
-			return res
-		} catch (e) {
-			console.log(e)
-			process.exit(-1)
-		}
-	}
-
-	const getRecentTweets = async (query: string, days?: number) => {
-		try {
-			const res = await searchRecentTweetsByQuery(query, days)
-			return res
-		} catch (e) {
-			console.log(e)
-			process.exit(-1)
-		}
-	}
-
-	const getMostPopTweetId = (tweets: Tweet[] | undefined) => {
-		console.log(`getMostPopTweetId`)
-
-		if (!tweets) {
-			console.log('getMostPopTweetId: tweets are undefined', tweets)
-			return
-		}
-
-		const ratingArr = tweets.map((tweet) => {
-			const rating = calcRating(tweet.public_metrics, weights)
-			return { id: tweet.id, rating }
-		})
-
-		const maxRating = ratingArr.reduce((acc: any, el: any) => {
-			return (acc = acc > el.rating ? acc : el.rating)
-		}, 0)
-
-		if (maxRating === 0) {
-			console.log('There were no tweets worth retweeting today.')
-			return
-		}
-
-		const mostPopTweet = ratingArr.find((el) => el.rating === maxRating)
-
-		console.log(ratingArr)
-		console.log(`The most popular tweet: ${mostPopTweet?.id} with a rating: ${maxRating}`)
-		return mostPopTweet?.id
-	}
-
+	let query = 
+		prompt('Enter query to search and retweet the most popular tweet with it:') 
+		|| 'как же мощно'
 	let recentTweets: { data: any[] } = { data: [] }
 
 	try {
-		recentTweets = await getRecentTweets('как же мощно')
+		recentTweets = await getRecentTweets(query)
 	} catch (e) {
 		console.log(e)
 	}
